0c60d867114d18bb1c80d430b11c19c6caf08421
yicrane <yicrane@qq.com>
2020-09-11 11:54:01 +0800

    ADD NOTE

--- Chapter3/ex_3_26.py
+++ Chapter3/ex_3_26.py
@@ -7,9 +7,10 @@ import torch.nn as nn
 # 深度残差网络基础代码
 def conv3x3(in_planes, out_planes, stride=1, groups=1, dilation=1):
     """3x3 convolution with padding"""
-    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
-                     padding=dilation, groups=groups, 
-bias=False, dilation=dilation)
+    return nn.Conv2d(in_planes, out_planes, kernel_size = 3, stride = stride,
+                     padding = dilation, groups = groups,
+                     bias = False, dilation = dilation)
+
 
 class BasicBlock(nn.Module):
     expansion = 1
